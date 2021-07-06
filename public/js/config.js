270acd884ec02369a8fe9a580ab52995705495cd
Nivriti <pnivriti3007@gmail.com>
2021-07-06 10:54:53 +0530

    first commit

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+turnConfig = {
+    iceServers: [
+        { 
+        "urls": ["stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+            ]
+        }
+    ]
+}
